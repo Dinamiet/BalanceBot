3b3f25ce5f89e10a3f79967932292345845562bb
Dinamiet <iceman.sm39@gmail.com>
2019-12-01 07:52:20 +0200

    Added some DMP initialization code

--- IMU.cpp
+++ IMU.cpp
@@ -2,10 +2,20 @@
 
 IMU::IMU(uint8_t address) { IMU_address = address; }
 
+void IMU::writeRegister(uint8_t reg, uint8_t value)
+{
+	Wire.beginTransmission(IMU_address);
+	Wire.write(reg);
+	Wire.write(value);
+	Wire.endTransmission(true);
+}
+
+void IMU::writeDMPMem() {}
+
 void IMU::init()
 {
 	initIMU();
-	initDMP();
+	// initDMP();
 }
 
 void IMU::initIMU()
@@ -13,25 +23,25 @@ void IMU::initIMU()
 	Wire.begin();
 	Wire.setClock(400000);
 
-	Wire.beginTransmission(IMU_address);
-	Wire.write(ACCEL_SETUP_REG);
-	Wire.write(ACCEL_SETUP_VAL);
-	Wire.endTransmission(true);
-
-	Wire.beginTransmission(IMU_address);
-	Wire.write(GYRO_SETUP_REG);
-	Wire.write(GYRO_SETUP_VAL);
-	Wire.endTransmission(true);
-
-	Wire.beginTransmission(IMU_address);
-	Wire.write(IMU_CLK_REG);
-	Wire.write(IMU_CLK_VAL);
-	Wire.endTransmission(true);
+	writeRegister(0x6B, 0x01); //Setup clock source
+	writeRegister(0x23, 0x00); //No FIFO
+	writeRegister(0x38, 0x00); //No INT
+	writeRegister(0x37, 0x80); //INT Active LOW
+	writeRegister(0x1C, 0x00); //Accelerometer SCALE
+	writeRegister(0x1B, 0x18); //Gyro SCALE
+	writeRegister(0x19, 0x04); //Sample RATE
+	writeRegister(0x1A, 0x01); //Digital Filter
 }
 
 void IMU::initDMP()
 {
+	writeDMPMem();
 
+	writeRegister(0x70, 0x04); //Program start Address
+	writeRegister(0x71, 0x00); //Program start Address
+
+	writeRegister(0x38, 0x02); //Enable INT
+	writeRegister(0x6A, 0xC4); //Enable DMP & FIFO and reset FIFO.
 }
 
 void IMU::getAccel(float* x, float* y, float* z)
@@ -61,7 +71,7 @@ void IMU::getGyro(float* x, float* y, float* z)
 void IMU::getTemp(float* temp)
 {
 	Wire.beginTransmission(IMU_address);
-	Wire.write(TEMP_START_REG);
+	Wire.write(RAW_TEMP_REG);
 	Wire.endTransmission(false);
 	Wire.requestFrom(IMU_address, 2);
 
--- IMU.h
+++ IMU.h
@@ -3,24 +3,12 @@
 
 #include "Wire.h"
 
-#define IMU_CLK_REG		0x6B
-#define ACCEL_SETUP_REG 0x1C
-#define GYRO_SETUP_REG	0x1B
-
 #define ACCEL_START_REG 0x3B
 #define GYRO_START_REG	0x43
 #define TEMP_START_REG	0x41
-#define INT_ENABLE_REG	0x38
-#define SAMPLE_RATE_REG	0x19
-
-#define IMU_CLK_VAL		0x01
-#define ACCEL_SETUP_VAL 0x00
-#define GYRO_SETUP_VAL	0x00
-#define INT_ENABLE_VAL	0x01
-#define SAMPLE_RATE_VAL	0xFF
 
 #define ACCEL_SCALE -16384.0f
-#define GYRO_SCALE	131.0f
+#define GYRO_SCALE	16.4f
 #define TEMP_SCALE	340.0f
 #define TEMP_OFFSET 36.53f
 
@@ -28,11 +16,13 @@ class IMU
 {
 	private:
 	int		IMU_address;
-	TwoWire Wire;	
+	TwoWire Wire;
+	void	writeRegister(uint8_t reg, uint8_t value);
+	void	writeDMPMem();
 
 	public:
 	IMU(uint8_t address);
-	void	init();
+	void init();
 	void enableINT();
 	void setSampleRate();
 	void getAccel(float* x, float* y, float* z);

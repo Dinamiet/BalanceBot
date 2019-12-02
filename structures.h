ba6a7203c82faa76cad47224a33abcb7edd7e62c
Dinamiet <iceman.sm39@gmail.com>
2019-12-02 17:38:38 +0200

    Added some data structures to help with the math

---
+++ structures.h
@@ -0,0 +1,19 @@
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+
+typedef struct
+{
+	float w;
+	float x;
+	float y;
+	float z;
+} Quaternion;
+
+typedef struct
+{
+	float x;
+	float y;
+	float z;
+} Vector;
+
+#endif

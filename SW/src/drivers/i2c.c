0c06b57d8f7855c89a8bc7ab5065966f2a9bb4cb
Dinamiet <iceman.sm39@gmail.com>
2024-10-18 20:35:50 +0200

    Faster I2C (400kHz)

--- SW/src/drivers/i2c.c
+++ SW/src/drivers/i2c.c
@@ -1,12 +1,12 @@
 #include "drivers.h"
 
-#define I2C_BUFFER_SIZE 64
+#define I2C_BUFFER_SIZE 32
 
 I2C* i2c;
 
 static uint8_t        i2cWriteBuff[I2C_BUFFER_SIZE];
 static uint8_t        i2cReadBuff[I2C_BUFFER_SIZE];
-static uint8_t        i2cTransBuff[3 * I2C_BUFFER_SIZE];
+static uint8_t        i2cTransBuff[2 * I2C_BUFFER_SIZE];
 
 void Setup_I2C()
 {
@@ -15,5 +15,5 @@ void Setup_I2C()
 	I2C_AssignTransactionBuffer(i2c, i2cTransBuff, sizeof(i2cTransBuff));
 	I2C_AssignReadBuffer(i2c, i2cReadBuff, sizeof(i2cReadBuff));
 	I2C_AssignWriteBuffer(i2c, i2cWriteBuff, sizeof(i2cWriteBuff));
-	I2C_SetBaud(i2c, 100000);
+	I2C_SetBaud(i2c, 400000);
 }

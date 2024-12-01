dfafae426dc8ed4aa191f49751ac6c2ec28c800a
Dinamiet <iceman.sm39@gmail.com>
2024-12-01 08:21:26 +0200

    Increase motor cooldown wait time and increase heartbeat frequency

--- SW/include/tasks.h
+++ SW/include/tasks.h
@@ -11,13 +11,13 @@
 #define TASK_MOTOR_STEP            6
 #define TASK_MOTORS_COOLDOWN       7
 #define TASK_MOTOR_STEP_PERIOD     0   // ms
-#define TASK_MOTORS_COOLDOWN_DELAY 200 // ms
+#define TASK_MOTORS_COOLDOWN_DELAY 500 // ms
 
 #define TASK_DELAY_CONTROL      8
 #define TASK_DELAY_CONTROL_TIME 1000 // ms
 
 #define TASK_HEARTBEAT        9
-#define TASK_HEARTBEAT_PERIOD 500 // ms
+#define TASK_HEARTBEAT_PERIOD 250 // ms
 
 #define TASK_CONFIG_RECEIVE        10
 #define TASK_CONFIG_RECEIVE_PERIOD 10 // ms
